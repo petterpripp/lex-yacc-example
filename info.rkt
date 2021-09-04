8b04897dff16b306c907ab5ab5ed23e94c774a9e
PetterPripp <petter.pripp@yahoo.com>
2021-09-04 14:37:31 +0200

    scribble  name same as project

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 
 (define collection  "lex-yacc-example")
-(define deps '("base" "parser-tools-lib" "sandbox-lib"))               
-(define build-deps '("racket-doc" "scribble-lib" "parser-tools-doc" ))
-(define scribblings '(("scribblings/lex-yacc.scrbl" ())))
+(define deps '("base" "parser-tools-lib" ))               
+(define build-deps '("racket-doc" "scribble-lib" "parser-tools-doc" "sandbox-lib"))
+(define scribblings '(("scribblings/lex-yacc-example.scrbl" ())))
 (define pkg-desc "Examples for lexer and yacc")
 (define pkg-authors '(Petter Pripp))
 (define test-omit-paths (if (getenv "PLT_PKG_BUILD_SERVICE") 'all '()))
